c44dc857464ca10eb54b348702749468c350e20f
Emon526 <emonats526@gmail.com>
2023-03-30 20:31:22 +0600

    trying node runtime

--- install-composer.sh
+++ install-composer.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+
+# Install PHP and cURL if they're not already installed
+if ! command -v php &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install -y php
+fi
+
+if ! command -v curl &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install -y curl
+fi
+
+# Download and install Composer
+EXPECTED_SIGNATURE="$(curl -sS https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
@@ -10,7 +25,7 @@ then
     exit 1
 fi
 
-php composer-setup.php --quiet
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer --quiet
 RESULT=$?
 rm composer-setup.php
 exit $RESULT
