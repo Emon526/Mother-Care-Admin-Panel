f430d54a17ef64e090096ae8572e67b0477dba9d
Emon526 <emonats526@gmail.com>
2023-03-30 19:57:29 +0600

    docker removed

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public'
-      },
     plugins: [
         laravel({
             input: [
