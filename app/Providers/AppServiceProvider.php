4c7c20c62a3e2cf2db4548e30d5b3a06d1be52b6
Emon526 <emonats526@gmail.com>
2023-04-05 02:38:04 +0600

    server deploy done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,15 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (config('app.env') === 'production') {
-        //     URL::forceScheme('https');
-        // }    
-        // if(config('app.env') === 'production') {
-        //     \URL::forceScheme('https');
-        // }    
-        // if ($this->app->environment('production')) {
-        //     $this->app['request']->server->set('HTTPS','on');
-        //     URL::forceSchema('https');
-        // }
+
     }
 }
